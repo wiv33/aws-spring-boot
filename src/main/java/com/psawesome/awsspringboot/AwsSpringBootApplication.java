c5f842b9ce7f67945740a7c00b13502980f16770
PSK <wiv.psk@gmail.com>
2019-12-25 15:00:31 +0900

    Initial commit

---
+++ src/main/java/com/psawesome/awsspringboot/AwsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.psawesome.awsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringBootApplication.class, args);
+    }
+
+}
