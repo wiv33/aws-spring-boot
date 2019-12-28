00becafcdccd8a46efa49b793653c5a2936385f3
PSK <wiv.psk@gmail.com>
2019-12-28 18:02:21 +0900

    # HelloControllerTest 성공 / @EnableJpaAuditing을 config로 옮긴다. WebMvcTest에서 @SpringBootApplication 을 읽기 때문에 분리시킨다. scan 비 대상 (@Repository, @Service, @Component)

---
+++ src/main/java/com/psawesome/awsspringboot/config/JpaConfig.java
@@ -0,0 +1,14 @@
+package com.psawesome.awsspringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * package: com.psawesome.awsspringboot.config
+ * author: PS
+ * DATE: 2019-12-28 토요일 17:59
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
