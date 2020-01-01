d8f7c2d463ed674c03fc7968cb93a03c729e8cd9
PSK <wiv.psk@gmail.com>
2020-01-01 16:14:02 +0900

    # aws 배포 test / build file

---
+++ build/resources/main/static/js.app/index.js
@@ -0,0 +1,72 @@
+const main = {
+    init: function () {
+        let _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+        $('#btn-update').on("click", function () {
+            _this.update();
+        });
+        $('#btn-delete').on('click', () => {
+            _this.deleteFunc();
+        })
+    },
+    save: () => {
+        let data = {
+            title: $("#title").val(),
+            author: $("#author").val(),
+            content: $("#content").val()
+        };
+
+        $.ajax({
+            url: '/api/v1/posts',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert("글이 등록되었습니다.");
+            window.location.href = '/';
+        }).fail((err) => {
+            alert(JSON.stringify(err))
+        });
+    },
+    update: () => {
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        };
+        let id = $("#id").val();
+
+        $.ajax({
+            url: '/api/v1/posts/' + id,
+            type: 'PUT',
+            dataType: 'JSON',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert("글이 수정되었습니다.");
+            window.location.href = '/';
+        }).catch((err) => {
+            console.log(err);
+            alert(JSON.stringify(err));
+        });
+    },
+    deleteFunc: () => {
+        let id = $('#id').val();
+
+        $.ajax({
+            url: '/api/v1/posts/' + id,
+            type: 'DELETE',
+            dataType: 'JSON',
+            contentType: "application/json; charset=UTF-8"
+        }).done(() => {
+            alert("글이 삭제되었습니다.");
+            window.location.href = '/';
+        }).catch((err) => {
+            console.log(JSON.stringify(err));
+        })
+    }
+};
+
+main.init();
